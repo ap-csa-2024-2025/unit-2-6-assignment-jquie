36604ffa01222e56c1c97fac4f1e58626ea1dd75
jquie <jacquelynphan03@gmail.com>
2024-10-14 19:06:22 -0700

    done !!

--- Main.java
+++ Main.java
@@ -4,6 +4,21 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner userInput = new Scanner (System.in);
+    System.out.println("input a double");
+    double radius = userInput.nextDouble();
+    Circle circle = new Circle(radius);
+    System.out.println(circle.getRadius());
+    System.out.println(circle.getCircumference());
+
+    System.out.println("input a int and a double");
+    int numOfSides = userInput.nextInt();
+    double sideLength = userInput.nextDouble();
+    RegularPolygon poly = new RegularPolygon (numOfSides, sideLength);
+    System.out.println(poly);
+    System.out.println(poly.getArea());
+    poly.addSides(2);
+    System.out.println(poly);
+    System.out.println(poly.getArea());
   }
 }
